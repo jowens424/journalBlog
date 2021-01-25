320528a933127140c3fcab0078e1634853d5b09e
jowens424 <jackowensii@gmail.com>
2021-01-24 23:55:23 -0500

    compose page

--- app.js
+++ app.js
@@ -16,9 +16,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
 
 app.get("/", function(req, res) {
-  res.render("home", {startingContent: homeStartingContent});
+  res.render("home", {startingContent: homeStartingContent,
+  posts: posts});
 });
 
 app.get("/about", function(req, res) {
@@ -29,6 +31,18 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactPage: contactContent});
 });
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+const post = {
+    title: req.body.postTitle,
+    content: req.body.newEntry
+  };
+  posts.push(post);
+res.redirect("/");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
